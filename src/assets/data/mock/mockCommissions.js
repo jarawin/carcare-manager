66a71444025ee7b37237eb46f511b57e3448655f
jarawin <newja2021@gmail.com>
2022-10-19 20:02:18 +0700

    OK

---
+++ src/assets/data/mock/mockCommissions.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    commission_id: "9ae640ad-92c2-4542-8201-f542c5149fc0",
+    topic: "คอมมิชชั่น 10 บาท",
+    description: "สำหรับออร์เดอร์ทั่วไป รับ 10 บาท ไม่เกิน 20 ออร์เดอร์ต่อวัน",
+    quota_type: "DAILY",
+    quota_amount: 20,
+    amount_type: "BATH",
+    amount: 10,
+    order_type: "GENERAL",
+  },
+  {
+    commission_id: "b814f9b9-fe4d-4b83-a22a-6dcb34b5faeb",
+    topic: "คอมมิชชั่น 100 บาท",
+    description:
+      "สำหรับออร์เดอร์สมาชิกพิเศษ รับ 5 บาท ไม่เกิน 10 ออร์เดอร์ต่อวัน",
+    quota_type: "DAILY",
+    quota_amount: 10,
+    amount_type: "BATH",
+    amount: 5,
+    order_type: "MEMBER",
+  },
+];
